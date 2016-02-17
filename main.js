9ed2025ef06e0cb72cbb62faec8030bdf01c52a5
Phil Skaggs <prskaggsjr@gmail.com>
2016-02-16 22:06:02 -0700

    Two tests passing

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 module.exports = {
   calcTax: function ( taxable ) {
 
+
+
+    if( taxable < 10 ){
+
     taxes = taxable * .10;
 
-    return taxes;
+  } else if ( 10 < taxable < 20){
 
+    var m = (10 * .10) + (taxable - 10) * .07;
+
+    taxes = Math.round(m * 100) / 100;
+
+  }
+    return taxes;
   }
 }
